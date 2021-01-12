8ddb1a0a4f6153e5aa8888fa0f5708d174c1206a
felix t <41817193+o2bomb@users.noreply.github.com>
2021-01-12 21:16:35 +0800

    Add map using Leaflet

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import "../sass/style.scss";
 
-console.log("hello world");
+import * as L from "leaflet";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const mymap = L.map("map").setView([51.505, -0.09], 13);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      accessToken: process.env.MAPBOX_PUBLIC_TOKEN,
+      tileSize: 512,
+      zoomOffset: -1,
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+    }
+  ).addTo(mymap);
+
+  console.log("hello world");
+}
