39944b83165abb04c6b4bbeb1c9e5c9a4f824fcc
felix t <41817193+o2bomb@users.noreply.github.com>
2021-01-13 00:45:24 +0800

    Remove zoom controls from map

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import * as L from "leaflet";
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  const mymap = L.map("map").setView([51.505, -0.09], 13);
+  const mymap = L.map("map", { zoomControl: false }).setView(
+    [51.505, -0.09],
+    13
+  );
 
   L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
