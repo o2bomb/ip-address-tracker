ce130cc74096b76f33a9fdc3ded275e70773a727
felix t <41817193+o2bomb@users.noreply.github.com>
2021-01-14 20:49:01 +0800

    Create build folder in server for serving files to browser

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,8 +23,8 @@ const main = async () => {
     res.send(data);
   })
 
+  app.use("/", express.static(path.join(__dirname, "..", "build")));
   if (process.env.NODE_ENV === "production") {
-    app.use("/", express.static(path.join(__dirname, "..", "client", "dist")));
   
     // app.get("*", (req, res) => {
     //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
